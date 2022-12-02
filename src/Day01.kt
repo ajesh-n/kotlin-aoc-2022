d67db9123fcea743bb930e7a5bf31a9d64553bc9
Ajesh Nair <ajesh.nair@thoughtworks.com>
2022-12-02 23:16:12 +0530

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,21 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: String): Int {
+        val elvesTotalFood =
+            input.split("\n\n").map { it.split("\n").sumOf { calories -> calories.toInt() } }
+        return elvesTotalFood.max()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: String): Int {
+        return input.split("\n\n").map { it.split("\n").sumOf { calories -> calories.toInt() } }
+            .sortedDescending().take(3).sum()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readText("Day01_test")
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
-    val input = readInput("Day01")
+    val input = readText("Day01")
     println(part1(input))
     println(part2(input))
 }
